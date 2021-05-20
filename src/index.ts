ba880debd9ff368a791b3122bc4b29d35343478f
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-05-20 12:51:26 +0530

    feat: Add contrasting color helper (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+export const getContrastingTextColor = (bgColor: string) => {
+  const color = bgColor.replace('#', '');
+  const r = parseInt(color.slice(0, 2), 16);
+  const g = parseInt(color.slice(2, 4), 16);
+  const b = parseInt(color.slice(4, 6), 16);
+  // http://stackoverflow.com/a/3943023/112731
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
 };
