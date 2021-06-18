0fd8b2e0296245e1556b82ba5150d546d546b05f
Muhsin <muhsinkeramam@gmail.com>
2021-06-18 13:48:54 +0530

    add format helpers

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,5 @@
-import { getContrastingTextColor } from '../src';
+import format from 'date-fns/format';
+import { getContrastingTextColor, formatDate } from '../src';
 
 describe('#getContrastingTextColor', () => {
   it('it should return white color code if #DF4CAB color passed  ', () => {
@@ -6,5 +7,40 @@ describe('#getContrastingTextColor', () => {
   });
   it('it should return black color code if #7DD5FF color passed  ', () => {
     expect(getContrastingTextColor('#7DD5FF')).toEqual('#000000');
+  });
+});
+
+describe('#formatDate', () => {
+  const today = new Date();
+  it('it should return formatted date if date is passed  ', () => {
+    expect(
+      formatDate({
+        date: 'Jun 09, 2021',
+        todayText: 'Today',
+        yesterdayText: 'Yesterday',
+      })
+    ).toEqual('Jun 09, 2021');
+  });
+  it('it should return yesterday if yesterday date is passed  ', () => {
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    const date = format(yesterday, 'MMM d, yyyy');
+    expect(
+      formatDate({
+        date,
+        todayText: 'Today',
+        yesterdayText: 'Yesterday',
+      })
+    ).toEqual('Yesterday');
+  });
+  it('it should return today if today date is passed  ', () => {
+    const date = format(today, 'MMM d, yyyy');
+    expect(
+      formatDate({
+        date,
+        todayText: 'Today',
+        yesterdayText: 'Yesterday',
+      })
+    ).toEqual('Today');
   });
 });
