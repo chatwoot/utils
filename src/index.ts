343e9c09037f51fca60254b3d2c28708aa7e83a5
Muhsin Keloth <muhsinkeramam@gmail.com>
2021-11-30 14:28:16 +0530

    feat: Add date helpers (#10)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
+import isToday from 'date-fns/isToday';
+import isYesterday from 'date-fns/isYesterday';
+
 /**
  * @name Get contrasting text color
- * @description Get contrasting text color  from a color
+ * @description Get contrasting text color from a text color
  * @param bgColor  Background color of text.
  * @returns contrasting text color
  */
-
-export const getContrastingTextColor = (bgColor: string) => {
+export const getContrastingTextColor = (bgColor: string): string => {
   const color = bgColor.replace('#', '');
   const r = parseInt(color.slice(0, 2), 16);
   const g = parseInt(color.slice(2, 4), 16);
   const b = parseInt(color.slice(4, 6), 16);
   // http://stackoverflow.com/a/3943023/112731
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
+};
+/**
+ * @name Get formatted date
+ * @description Get date in today, yesterday or any other date format
+ * @param date  date
+ * @param todayText  Today text
+ * @param yesterdayText  Yesterday text
+ * @returns formatted date
+ */
+export const formatDate = ({
+  date,
+  todayText,
+  yesterdayText,
+}: {
+  date: string;
+  todayText: string;
+  yesterdayText: string;
+}) => {
+  const dateValue = new Date(date);
+  if (isToday(dateValue)) return todayText;
+  if (isYesterday(dateValue)) return yesterdayText;
+  return date;
 };
