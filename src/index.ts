ff2d3bc853ded4024a7c5720af7ed8aa1ae97614
Aswin Dev P.S <aswindevps@gmail.com>
2022-02-28 13:02:45 +0530

    feat: Add time format helper (#11)
    Co-authored-by: Pranav Raj S <pranav@chatwoot.com>

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export const getContrastingTextColor = (bgColor: string): string => {
   // http://stackoverflow.com/a/3943023/112731
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
 };
+
 /**
  * @name Get formatted date
  * @description Get date in today, yesterday or any other date format
@@ -36,4 +37,37 @@ export const formatDate = ({
   if (isToday(dateValue)) return todayText;
   if (isYesterday(dateValue)) return yesterdayText;
   return date;
+};
+
+/**
+ * @name formatTime
+ * @description Format time to Hour, Minute and Second
+ * @param timeInSeconds  number
+ * @returns formatted time
+ */
+
+export const formatTime = (timeInSeconds: number) => {
+  let formattedTime = '';
+  if (timeInSeconds >= 60 && timeInSeconds < 3600) {
+    return `${Math.round(timeInSeconds / 60)} Min`;
+  }
+  if (timeInSeconds >= 3600 && timeInSeconds < 86400) {
+    const hours = Math.round(timeInSeconds / 3600);
+    formattedTime = `${hours} Hr`;
+    const minutes =
+      timeInSeconds % 3600 < 60 || hours === 24
+        ? 0
+        : Math.round((timeInSeconds % 3600) / 60);
+    return formattedTime + `${minutes > 0 ? ' ' + minutes + ' Min' : ''}`;
+  }
+  if (timeInSeconds >= 86400) {
+    const days = Math.round(timeInSeconds / 86400);
+    formattedTime = `${days} Day`;
+    const hours =
+      timeInSeconds % 86400 < 3600 || days >= 364
+        ? 0
+        : Math.round((timeInSeconds % 86400) / 3600);
+    return formattedTime + `${hours > 0 ? ' ' + hours + ' Hr' : ''}`;
+  }
+  return `${Math.round(timeInSeconds)} Sec`;
 };
