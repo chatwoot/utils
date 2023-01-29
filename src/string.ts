1503b08c7451506a14479b041ba52c1acecc98ca
Shivam Mishra <scm.mymail@gmail.com>
2023-01-29 11:08:59 +0530

    chore: allow wider boolean representations

--- src/string.ts
+++ src/string.ts
@@ -6,11 +6,12 @@
 
 export function parseBoolean(candidate: string | number) {
   try {
+    // lowercase the string, so TRUE becomes true
+    const candidateString = String(candidate).toLowerCase();
+
     // wrap in boolean to ensure that the return value
     // is a boolean even if values like 0 or 1 are passed
-    return Boolean(
-      JSON.parse(typeof candidate === 'number' ? String(candidate) : candidate)
-    );
+    return Boolean(JSON.parse(candidateString));
   } catch (error) {
     return false;
   }
