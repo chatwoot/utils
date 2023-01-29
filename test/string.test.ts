c5d90f5f48a52dcc238bc3c7462c12420fb5eb6f
Shivam Mishra <scm.mymail@gmail.com>
2023-01-29 11:02:49 +0530

    feat: add parseBoolean

---
+++ test/string.test.ts
@@ -0,0 +1,33 @@
+import { parseBoolean } from '../src';
+
+describe('#parseBoolean', () => {
+  test('returns true for input "true"', () => {
+    expect(parseBoolean('true')).toBe(true);
+  });
+
+  test('returns false for input "false"', () => {
+    expect(parseBoolean('false')).toBe(false);
+  });
+
+  test('returns true for input "1"', () => {
+    expect(parseBoolean(1)).toBe(true);
+  });
+
+  test('returns false for input "0"', () => {
+    expect(parseBoolean(0)).toBe(false);
+  });
+
+  test('returns false for input "non-boolean value"', () => {
+    expect(parseBoolean('non-boolean value')).toBe(false);
+  });
+
+  test('returns false for input "null"', () => {
+    // @ts-ignore
+    expect(parseBoolean(null)).toBe(false);
+  });
+
+  test('returns false for input "undefined"', () => {
+    // @ts-ignore
+    expect(parseBoolean(undefined)).toBe(false);
+  });
+});
