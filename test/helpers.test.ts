70487027c60862037d355c43d32f561b97325e32
Vishnu Narayanan <vishnu@chatwoot.com>
2024-02-22 18:13:03 +0530

    chore: improve spec coverage

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -15,4 +15,32 @@ describe('#convertSecondsToTimeUnit', () => {
       })
     ).toEqual({ time: 1, unit: 'Minutes' });
   });
+  it("it should return { time:  1, unit: 'h' } if  3600 seconds passed", () => {
+    expect(
+      convertSecondsToTimeUnit(3600, { minute: 'm', hour: 'h', day: 'd' })
+    ).toEqual({ time: 1, unit: 'h' });
+  });
+  it("it should return { time:  1, unit: 'Hours' } if  3600 seconds passed", () => {
+    expect(
+      convertSecondsToTimeUnit(3600, {
+        minute: 'Minutes',
+        hour: 'Hours',
+        day: 'Days',
+      })
+    ).toEqual({ time: 1, unit: 'Hours' });
+  });
+  it("it should return { time:  1, unit: 'd' } if  86400 seconds passed", () => {
+    expect(
+      convertSecondsToTimeUnit(86400, { minute: 'm', hour: 'h', day: 'd' })
+    ).toEqual({ time: 1, unit: 'd' });
+  });
+  it("it should return { time:  1, unit: 'Days' } if  86400 seconds passed", () => {
+    expect(
+      convertSecondsToTimeUnit(86400, {
+        minute: 'Minutes',
+        hour: 'Hours',
+        day: 'Days',
+      })
+    ).toEqual({ time: 1, unit: 'Days' });
+  });
 });
